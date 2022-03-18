aea02f1c62babab2f594f7f3aa42ab27a6948a61
Ali Almasi <A710almasi@gmail.com>
2022-03-18 19:57:17 +0330

    assemblyinfo file changed

--- Notepad/Properties/AssemblyInfo.cs
+++ Notepad/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Notepad Redesigned by Ali Almasi")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ali Almasi")]
-[assembly: AssemblyProduct("Notepad")]
+[assembly: AssemblyProduct("Notepad - Almasi")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("Ali Almasi")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]

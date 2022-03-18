237384c92245f1fe00702006acea6e156dcef1e7
Ali Almasi <A710almasi@gmail.com>
2022-03-18 19:48:15 +0330

    form1 start position changed

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -213,6 +213,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(300, 200);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Notepad - Almasi";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
